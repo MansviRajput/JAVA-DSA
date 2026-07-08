c4353632840d8e802a814161a787e3daa2a951a7
Mansvi Rajput <mansvirajput488@gmail.com>
2026-07-08 23:51:50 +0530

    Daily Problem

---
+++ untitled/src/Striver/DSA/Sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package Striver.DSA.Sort;
+
+// selection sort is about comparing two element from each o
+
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {64, 25, 12, 22, 11};
+        System.out.println(Arrays.toString(selectionSort(nums)));
+    }
+
+    private static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        return arr;
+    }
+}
