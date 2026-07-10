812d0299b691de9fd7bc8eaa4d125ee7a95a17ee
Mansvi Rajput <mansvirajput488@gmail.com>
2026-07-10 23:18:49 +0530

    Daily Problem

---
+++ untitled/src/Striver/DSA/Arrays/secondLargest.java
@@ -0,0 +1,17 @@
+//package Striver.DSA.Arrays;
+//
+//public class secondLargest {
+//    public static void main(String[] args) {
+//        int[] nums = {7,4,8,9,10};
+//
+//    }
+//
+//    private static int secondLargest(int[] nums) {
+//        int max = Integer.MIN_VALUE;
+//        for(int i=0;i<nums.length;i++){
+//            if(first>nums[i+1]){
+//
+//            }
+//        }
+//    }
+//}
