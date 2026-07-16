2085d8747c7a74335d119024a69a1e62d0c175d0
Mansvi Rajput <mansvirajput488@gmail.com>
2026-07-16 12:36:58 +0530

    16 July noon

---
+++ untitled/src/Striver/DSA/Arrays/setMatrixZeroes.java
@@ -0,0 +1,40 @@
+package Striver.DSA.Arrays;
+
+import java.util.Arrays;
+
+public class setMatrixZeroes {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0,1,2,0},
+                {3,4,5,2},
+                {1,3,1,5}
+        };
+        setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    private static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
