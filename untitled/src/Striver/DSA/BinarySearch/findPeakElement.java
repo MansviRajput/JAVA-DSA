7aa93ea90f17334a3d7b143896f518f502cd9b59
Mansvi Rajput <mansvirajput488@gmail.com>
2026-07-28 20:50:54 +0530

    28 July eveninnn

---
+++ untitled/src/Striver/DSA/BinarySearch/findPeakElement.java
@@ -0,0 +1,46 @@
+package Striver.DSA.BinarySearch;
+
+public class findPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
+        System.out.println(findPeak1(nums));
+    }
+
+    private static int findPeak1(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] > nums[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    private static int findPeak2(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = start+(end-start)/2;
+            boolean left =
+                    mid==0 || nums[mid]>nums[mid-1];
+            boolean right =
+                    mid==nums.length-1 || nums[mid]>nums[mid+1];
+            if(left && right){
+                return mid;
+            }
+            if(mid < nums.length-1 &&
+                    nums[mid] < nums[mid+1]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
--- untitled/src/Striver/DSA/BinarySearch/singleElementInSortedArray.java
+++ untitled/src/Striver/DSA/BinarySearch/singleElementInSortedArray.java
@@ -2,13 +2,62 @@ package Striver.DSA.BinarySearch;
 
 public class singleElementInSortedArray {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,8};
-        System.out.println(solution(nums));
+        int[] nums = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+        System.out.println(solution1(nums));
     }
 
-//    private static int solution1(int[] nums){
-//
-//    }
+    private static int solution2(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if(mid % 2 == 1){
+                mid--;
+            }
+
+            if(nums[mid] == nums[mid + 1]){
+                start = mid + 2;
+            }else{
+                end = mid;
+            }
+        }
+        return nums[start];
+    }
+
+    private static int solution1(int[] nums){
+
+        if (nums.length == 1) return nums[0];
+
+        if (nums[0] != nums[1]) return nums[0];
+
+        if (nums[nums.length - 1] != nums[nums.length - 2])
+            return nums[nums.length - 1];
+
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid < nums.length-1 && nums[mid]==nums[mid+1]){
+                 if(mid % 2 == 0){
+                     start=mid+2;
+                 }else{
+                     end=mid-1;
+                 }
+            }
+            else if(mid > 0 && nums[mid]==nums[mid-1]){
+                if(mid % 2 != 0){
+                    start=mid+1;
+                }else{
+                    end=mid-2;
+                }
+            }
+        }
+        return -1;
+    }
 
     private static int solution(int[] nums){
         int xor = 0;
@@ -17,4 +66,10 @@ public class singleElementInSortedArray {
         }
         return xor;
     }
+
+
+
+
+
+
 }
